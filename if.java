f93b2db1204c4871cd21eb8375f3bc26dc6027bc
garvitnamdev <garvit_namdev@srmuniv.edu.in>
2017-07-09 15:13:26 +0530

    Add files via upload

---
+++ if.java
@@ -0,0 +1,11 @@
+
+{
+System.out.println('6 is greater');
+}
+Here , the expression 6 >
+3 is evaluated. Since 6 is greater than 3, the expression is true, hence the code inside the ?if? block gets executed. 
+Similarly we can look at our second example
+if(6 < 4)
+{
+System.out.println('4 is greatest');
+}
