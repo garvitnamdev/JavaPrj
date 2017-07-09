8fced403889d88f5f1e3802eb5ab952590666c95
garvitnamdev <garvit_namdev@srmuniv.edu.in>
2017-07-09 15:23:02 +0530

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,10 @@
+
+Compile and run the following example and try to understand the output:
+class MyProgram {
+public static void main(String args[])
+{
+System.out.println(1+3+5+7+9);
+System.out.println(35/7);
+System.out.println(10*9+10*9);
+}
+} 
